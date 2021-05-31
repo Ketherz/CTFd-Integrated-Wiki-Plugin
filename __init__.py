36fa99e040b149a09a0f30f65a1b29a1f2540326
Ketherz <edmond.cuillier@gmail.com>
2021-05-31 15:31:57 +0000

    page de guarde wiki

--- __init__.py
+++ __init__.py
@@ -1,7 +1,22 @@
-from flask import render_template
+from flask import render_template, Blueprint
+from CTFd.plugins import register_plugin_assets_directory
+import os
+
+# TODO: It will be better to use the database to store everyfiles, but we will see that later
+""" This function is used to display all the files containing soluces the solutions are in the folder soluce """
+
+def define_wiki(app) :
+    wiki_blueprint = Blueprint('wiki', __name__, template_folder='templates', static_folder='assets')
+    @wiki_blueprint.route("/wiki", methods=["GET"])
+    def wiki():
+        soluce_file = []
+        directory = os.path.dirname(os.path.realpath(__file__)) + "/soluce"
+        for filename in os.listdir(directory) :
+            soluce_file.append(filename)
+
+        return render_template('wiki.html', soluce_file = soluce_file, title = "Wiki")
+    app.register_blueprint(wiki_blueprint)
 
 
 def load(app):
-    @app.route('/wiki', methods=['GET'])
-    def view_wiki():
-        return render_template('page.html', content="<h1>Wiki</h1>")
+    define_wiki(app)
